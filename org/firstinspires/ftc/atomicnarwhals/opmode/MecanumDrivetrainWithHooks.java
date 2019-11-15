cd3e40b4aa2cfe6b7031a912279db2440aaf7081
rahulsantha2006 <30245954+rahulsantha2006@users.noreply.github.com>
2019-11-15 14:50:10 -0600

    Add files via upload

--- org/firstinspires/ftc/atomicnarwhals/opmode/MecanumDrivetrainWithHooks.java
+++ org/firstinspires/ftc/atomicnarwhals/opmode/MecanumDrivetrainWithHooks.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.atomicnarwhals.hardware.DraggerServo;
 import org.firstinspires.ftc.atomicnarwhals.hardware.IntakeWheels;
 import org.firstinspires.ftc.atomicnarwhals.hardware.MecanumDrivetrain;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Mecanum 1", group="Linear Opmode")
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Mecanum 2", group="Linear Opmode")
 public class MecanumDrivetrainTeleOp extends LinearOpMode {
 
     private static final double THROTTLE = 0.35;
@@ -27,8 +27,14 @@ public class MecanumDrivetrainTeleOp extends LinearOpMode {
         DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
         DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-
+        
         mMecanumDrivetrain = new MecanumDrivetrain(telemetry, frontLeft, frontRight, backLeft, backRight);
+
+        CRServo hook1  = hardwareMap.get(CRServo.class, "hook1");
+        CRServo hook2  = hardwareMap.get(CRServo.class, "hook2");
+
+        DraggerServo hookServo1 = new DraggerServo(hook1);
+        DraggerServo hookServo2 = new DraggerServo(hook2);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -40,10 +46,22 @@ public class MecanumDrivetrainTeleOp extends LinearOpMode {
                                     -gamepad1.left_stick_y, 
                                     gamepad1.right_stick_x, 
                                     THROTTLE);
+
+            if (gamepad1.x) {
+                hookServo1.moveDown();
+                hookServo2.moveDown();
+            }
+            else if (gamepad1.y) {
+                hookServo1.moveUp();
+                hookServo2.moveUp();
+            }
+            else {
+                hookServo1.stop ();
+                hookServo2.stop ();
+            }
         }
 
         telemetry.addData("MecanumDrivetrainTeleOp", "Stopping");
-
         mMecanumDrivetrain.stop();
     }
 }
